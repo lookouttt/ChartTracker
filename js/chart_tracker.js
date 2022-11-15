3d182553e3ec8f36b05e3caf56bfaa058cb55bc0
Jeff Turner <marinerjeff@gmail.com>
2022-11-15 01:33:15 -0800

    Got all chart types loading correctly.  Got datepicker working.

--- js/chart_tracker.js
+++ js/chart_tracker.js
@@ -8,107 +8,107 @@ const max_decade = 2020;
 let charts_general = [
     ['hot-100','Hot 100','1958-08-04','2022-11-05'],
     ['billboard-200','Billboard 200','1963-08-17','2022-11-05'],
-    ['bubbling-under-hot-100-singles','Bubbling Under',1992,'2022-11-05'],
-    ['hot-singles-reccurents','Hot Singles Recurrents',1991,'2022-11-05'],
-    ['streaming-songs','Streaming Songs',2013,'2022-11-05'],
-    ['radio-songs','Radio Songs',1990,'2022-11-05'],
-    ['digital-song-sales','Digital Song Sales',2004,'2022-11-05'],
-    ['top-album-sales','Top Album Sales',1991,'2022-11-05'],
-    ['current-albums','Current Albums',1983,'2022-11-05']
+    ['bubbling-under-hot-100-singles','Bubbling Under','1992-12-05','2022-11-05'],
+    ['hot-singles-recurrents','Hot Singles Recurrents','1991-11-30','2022-11-05'],
+    ['streaming-songs','Streaming Songs','2013-01-26','2022-11-05'],
+    ['radio-songs','Radio Songs','1990-11-03','2022-11-05'],
+    ['digital-song-sales','Digital Song Sales','2004-10-30','2022-11-05'],
+    ['top-album-sales','Top Album Sales','1991-05-25','2022-11-05'],
+    ['current-albums','Current Albums','1983-11-05','2022-11-05']
 ];
 
 let charts_rock = [
-    ['rock-songs','Rock Songs',2009,'2022-11-05'],
-    ['rock-albums','Rock Albums',2006,'2022-11-05'],
-    ['rock-streaming-songs','Rock Streaming Songs',2013,'2022-11-05'],
-    ['rock-airplay','Rock Airplay',2009,'2022-11-05'],
-    ['rock-digital-song-sales','Rock Digital Song Sales',2010,'2022-11-05'],
-    ['hot-mainstream-rock-tracks','Hot Mainstream Rock Tracks',1981,'2022-11-05'],
-    ['hot-hard-rock-songs','Hot Hard Rock Songs',2020,'2022-11-05'],
-    ['hard-rock-albums','Hard Rock Albums',2007,'2022-11-05'],
-    ['hard-rock-streaming-songs','Hard Rock Streaming Songs',2020,'2022-11-05'],
-    ['hard-rock-digital-song-sales','Hard Rock Digital Song Sales',2011,'2022-11-05']
+    ['rock-songs','Rock Songs','2009-06-20','2022-11-05'],
+    ['rock-albums','Rock Albums','2006-01-14','2022-11-05'],
+    ['rock-streaming-songs','Rock Streaming Songs','2013-04-20','2022-11-05'],
+    ['rock-airplay','Rock Airplay','2009-06-20','2022-11-05'],
+    ['rock-digital-song-sales','Rock Digital Song Sales','2010-01-23','2022-11-05'],
+    ['hot-mainstream-rock-tracks','Hot Mainstream Rock Tracks','1981-03-21','2022-11-05'],
+    ['hot-hard-rock-songs','Hot Hard Rock Songs','2020-06-13','2022-11-05'],
+    ['hard-rock-albums','Hard Rock Albums','2007-07-28','2022-11-05'],
+    ['hard-rock-streaming-songs','Hard Rock Streaming Songs','2020-06-13','2022-11-05'],
+    ['hard-rock-digital-song-sales','Hard Rock Digital Song Sales','2011-01-22','2022-11-05']
 ];
 
 let charts_country = [
-    ['country-songs','Country Songs',1958,'2022-11-05'],
-    ['country-albums','Country Albums',1964,'2022-11-05'],
-    ['country-streaming-songs','Country Streaming Songs',2013,'2022-11-05'],
-    ['country-airplay','Country Airplay',1990,'2022-11-05'],
-    ['country-digital-song-sales','Country Digital Song Sales',2010,'2022-11-05'],
-    ['americana-folk-albums','Americana Folk Albums',2009,'2022-11-05']
+    ['country-songs','Country Songs','1958-10-20','2022-11-05'],
+    ['country-albums','Country Albums','1964-01-11','2022-11-05'],
+    ['country-streaming-songs','Country Streaming Songs','2013-04-20','2022-11-05'],
+    ['country-airplay','Country Airplay','1990-01-20','2022-11-05'],
+    ['country-digital-song-sales','Country Digital Song Sales','2010-01-23','2022-11-05'],
+    ['americana-folk-albums','Americana Folk Albums','2009-12-05','2022-11-05']
 ];
 
 let charts_jazz = [
-    ['jazz-albums','Jazz Albums',1993,'2022-11-05'],
-    ['contemporary-jazz','Contemporary Jazz',1987,'2022-11-05'],
-    ['traditional-jazz-albums','Traditional Jazz Albums',1983,'2022-11-05'],
-    ['jazz-songs','Jazz Songs',2005,'2022-11-05']
+    ['jazz-albums','Jazz Albums','1993-11-13','2022-11-05'],
+    ['contemporary-jazz','Contemporary Jazz','1987-02-28','2022-11-05'],
+    ['traditional-jazz-albums','Traditional Jazz Albums','1983-11-12','2022-11-05'],
+    ['jazz-songs','Jazz Songs','2005-10-22','2022-11-05']
 ];
 
 let charts_alternative = [
-    ['hot-alternative-songs','Hot Alternative Songs',2020,'2022-11-05'],
-    ['alternative-albums','Alternative Albums',2007,'2022-11-05'],
-    ['alternative-streaming-songs','Alternative Streaming Songs',2020,'2022-11-05'],
-    ['alternative-airplay','Alternative Airplay',1988,'2022-11-05'],
-    ['alternative-digital-song-sales','Alternative Digital Song Sales',2011,'2022-11-05'],
-    ['triple-a','Triple A',1996,'2022-11-05']
+    ['hot-alternative-songs','Hot Alternative Songs','2020-06-13','2022-11-05'],
+    ['alternative-albums','Alternative Albums','2007-07-28','2022-11-05'],
+    ['alternative-streaming-songs','Alternative Streaming Songs','2020-06-13','2022-11-05'],
+    ['alternative-airplay','Alternative Airplay','1988-09-10','2022-11-05'],
+    ['alternative-digital-song-sales','Alternative Digital Song Sales','2011-01-22','2022-11-05'],
+    ['triple-a','Triple A','1996-01-20','2022-11-05']
 ];
 
 let charts_christian = [
-    ['christian-songs','Christian Songs',2003,'2022-11-05'],
-    ['christian-albums','Christian Albums',1983,'2022-11-05'],
-    ['christian-streaming-songs','Christian Streaming Songs',2013,'2022-11-05'],
-    ['christian-airplay','Christian Airplay',2003,'2022-11-05'],
-    ['christian-digital-song-sales','Christian Digital Song Sales',2010,'2022-11-05'],
-    ['hot-christian-adult-contemporary','Hot Christian Adult Contemporary',2003,'2022-11-05'],
-    ['gospel-songs','Gospel Songs',2005,'2022-11-05'],
-    ['gospel-albums','Gospel Albums',1983,'2022-11-05'],
-    ['gospel-streaming-songs','Gospel Streaming Songs',,'2022-11-05'],
-    ['gospel-airplay','Gospel Airplay',2005,'2022-11-05'],
-    ['gospel-digital-song-sales','Gospel Digital Song Sales',2010,'2022-11-05']
+    ['christian-songs','Christian Songs','2003-06-21','2022-11-05'],
+    ['christian-albums','Christian Albums','1983-11-12','2022-11-05'],
+    ['christian-streaming-songs','Christian Streaming Songs','2013-12-07','2022-11-05'],
+    ['christian-airplay','Christian Airplay','2003-06-21','2022-11-05'],
+    ['christian-digital-song-sales','Christian Digital Song Sales','2010-01-23','2022-11-05'],
+    ['hot-christian-adult-contemporary','Hot Christian Adult Contemporary','2003-06-21','2022-11-05'],
+    ['gospel-songs','Gospel Songs','2005-03-19','2022-11-05'],
+    ['gospel-albums','Gospel Albums','1983-11-26','2022-11-05'],
+    ['gospel-streaming-songs','Gospel Streaming Songs','2013-12-07','2022-11-05'],
+    ['gospel-airplay','Gospel Airplay','2005-03-19','2022-11-05'],
+    ['gospel-digital-song-sales','Gospel Digital Song Sales','2010-01-23','2022-11-05']
 ];
 
 let charts_misc = [
-    ['dance-club-play-songs','Dance Club Play Songs',1976, '2020-03-28'],
-    ['heatseekers-albums','Heatseekers Albums',1991,'2022-11-05'],
-    ['blues-albums','Blues Albums',1995,'2022-11-05'],
-    ['tastemaker-albums','Tastemaker Albums',2005,'2022-11-05'],
-    ['billboard-global-200','Global 200',2020,'2022-11-05'],
-    ['billboard-global-excl-us','Global Exclduing U.S.',2020,'2022-11-05'],
-    ['catalog-albums','Catalog Albums',1991,'2022-11-05'],
-    ['independent-albums','Independent Albums',2000,'2022-11-05'],
-    ['vinyl-albums','Vinyl Albums',2011,'2022-11-05']
+    ['dance-club-play-songs','Dance Club Play Songs','1976-08-28', '2020-03-28'],
+    ['heatseekers-albums','Heatseekers Albums','1991-07-13','2022-11-05'],
+    ['blues-albums','Blues Albums','1995-09-02','2022-11-05'],
+    ['tastemaker-albums','Tastemaker Albums','2005-12-03','2022-11-05'],
+    ['billboard-global-200','Global 200','2020-09-19','2022-11-05'],
+    ['billboard-global-excl-us','Global Exclduing U.S.','2020-09-19','2022-11-05'],
+    ['catalog-albums','Catalog Albums','1991-05-25','2022-11-05'],
+    ['independent-albums','Independent Albums','2000-01-29','2022-11-05'],
+    ['vinyl-albums','Vinyl Albums','2011-01-22','2022-11-05']
 ];
 
 let charts_pop = [
-    ['pop-songs','Pop Songs',1992,'2022-11-05'],
-    ['adult-contemporary','Adult Contemporary',1961,'2022-11-05'],
-    ['adult-pop-songs','Adult Pop Songs',1995,'2022-11-05']
+    ['pop-songs','Pop Songs','1992-10-03','2022-11-05'],
+    ['adult-contemporary','Adult Contemporary','1961-07-17','2022-11-05'],
+    ['adult-pop-songs','Adult Pop Songs','1995-10-07','2022-11-05']
 ];
 
 let charts_r_b = [
-    ['r-b-hip-hop-songs','R&B / Hip-Hop Songs',1958,'2022-11-05'],
-    ['r-b-hip-hop-albums','R&B / Hip-Hop Albums',1965,'2022-11-05'],
-    ['r-and-b-hip-hop-digital-song-sales','R&B / Hip-Hop Digital Song Sales',2010,'2022-11-05'],
-    ['r-and-b-streaming-songs','R&B Streaming Songs',2013,'2022-11-05'],
-    ['mainstream-r-and-b-hip-hop','Mainstream R&B / Hip-Hop',1993,'2022-11-05'],
-    ['hot-adult-r-and-b-airplay','Hot Adult R&B Airplay',1993,'2022-11-05']
+    ['r-b-hip-hop-songs','R&B / Hip-Hop Songs','1958-10-20','2022-11-05'],
+    ['r-b-hip-hop-albums','R&B / Hip-Hop Albums','1965-01-30','2022-11-05'],
+    ['r-and-b-hip-hop-digital-song-sales','R&B / Hip-Hop Digital Song Sales','2010-01-23','2022-11-05'],
+    ['r-and-b-streaming-songs','R&B Streaming Songs','2013-04-20','2022-11-05'],
+    ['mainstream-r-and-b-hip-hop','Mainstream R&B / Hip-Hop','1993-09-18','2022-11-05'],
+    ['hot-adult-r-and-b-airplay','Hot Adult R&B Airplay','1993-09-18','2022-11-05']
 ];
 
 let top_charts = [
-    ['hot-100','Hot 100',1958,'2022-11-05'],
-    ['billboard-200','Billboard 200',1963,'2022-11-05'],
-    ['bubbling-under-hot-100-singles','Bubbling Under',1992,'2022-11-05'],
-    ['hot-singles-reccurents','Hot Singles Recurrents',1991,'2022-11-05'],
-    ['adult-contemporary','Adult Contemporary',1961,'2022-11-05'],
-    ['hot-mainstream-rock-tracks','Hot Mainstream Rock Tracks',1981,'2022-11-05'],
-    ['country-songs','Country Songs',1958,'2022-11-05'],
-    ['country-albums','Country Albums',1964,'2022-11-05'],
-    ['r-b-hip-hop-songs','R&B / Hip-Hop Songs',1958,'2022-11-05'],
-    ['r-b-hip-hop-albums','R&B / Hip-Hop Albums',1965,'2022-11-05'],
-    ['alternative-airplay','Alternative Airplay',1988,'2022-11-05'],
-    ['triple-a','Triple A',1996,'2022-11-05']
+    ['hot-100','Hot 100','1958-08-04','2022-11-05'],
+    ['billboard-200','Billboard 200','1963-08-17','2022-11-05'],
+    ['bubbling-under-hot-100-singles','Bubbling Under','1992-12-05','2022-11-05'],
+    ['hot-singles-recurrents','Hot Singles Recurrents','1991-11-30','2022-11-05'],
+    ['adult-contemporary','Adult Contemporary','1961-07-17','2022-11-05'],
+    ['hot-mainstream-rock-tracks','Hot Mainstream Rock Tracks','1981-03-21','2022-11-05'],
+    ['country-songs','Country Songs','1958-10-20','2022-11-05'],
+    ['country-albums','Country Albums','1964-01-11','2022-11-05'],
+    ['r-b-hip-hop-songs','R&B / Hip-Hop Songs','1958-10-20','2022-11-05'],
+    ['r-b-hip-hop-albums','R&B / Hip-Hop Albums','1965-01-30','2022-11-05'],
+    ['alternative-airplay','Alternative Airplay','1988-09-10','2022-11-05'],
+    ['triple-a','Triple A','1996-01-20','2022-11-05']
 ]
 
 let genre_list = [
@@ -252,3 +252,14 @@ function populateYearSelection(first_date, last_date, html_link) {
     }
     
 }
+
+function openDatepickerSelection(selectedDate) {
+    let myChart = document.getElementById('fc-datepicker').getAttribute('data-chart');
+    let myChartName = document.getElementById('fc-datepicker').getAttribute('data-chartName');
+    let myFirst = document.getElementById('timeframeDrop').getAttribute('data-firstDate');
+    let myLast = document.getElementById('timeframeDrop').getAttribute('data-lastDate');
+    let myGenre = document.getElementById('chartDrop').getAttribute('data-genre');
+    let myHtmlLink = "fullchart.html?chart=" + myChart + "&cname=" + myChartName + "&first=" + myFirst + "&last=" + myLast + "&date=" + selectedDate + "&genre=" + myGenre;
+    window.open(myHtmlLink, "_self");
+}
+
