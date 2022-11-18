9eb762354b06a8b7f421ab7f741c3764ee8947a4
Jeff Turner <marinerjeff@gmail.com>
2022-11-18 02:07:01 -0800

    Added a carousel and the about page.

--- js/main.js
+++ js/main.js
@@ -3,20 +3,34 @@ $(function() {
 
     let pDate1 = charts_general[0][chart_most_recent];
     let printDate = new Date(pDate1);
-    let titleString = "Hot 100 for Week Ending " + printDate.toLocaleDateString('en-US', {timeZone: "UTC"});
+    let titleString = "Top 10 from the Hot 100 for Week Ending " + printDate.toLocaleDateString('en-US', {timeZone: "UTC"});
     let myTitle = document.getElementById("h100-title");
     myTitle.innerHTML = titleString;
     
     let pDate2 = charts_general[1][chart_most_recent];
     printDate = new Date(pDate2);
-    titleString = "Billboard 200 for Week Ending " + printDate.toLocaleDateString('en-US', {timeZone: "UTC"});
+    titleString = "Top 10 from the Billboard 200 for Week Ending " + printDate.toLocaleDateString('en-US', {timeZone: "UTC"});
     myTitle = document.getElementById("b200-title");
     myTitle.innerHTML = titleString;
     
+    $(".carousel").carousel( { interval: 5000 });
+
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+
     //initializing the table
     CsvToHtmlTable.init({
         csv_path: 'data/hot-100/hot-100_' + pDate1 + '.csv', 
-        row_limit: 20,
+        row_limit: 10,
         element: 'hot100-container', 
         allow_download: true,
         csv_options: {separator: ',', delimiter: '"'},
@@ -25,7 +39,7 @@ $(function() {
     });
     CsvToHtmlTable.init({
         csv_path: 'data/billboard-200/billboard-200_' + pDate2 + '.csv', 
-        row_limit: 20,
+        row_limit: 10,
         element: 'billboard200-container', 
         allow_download: true,
         csv_options: {separator: ',', delimiter: '"'},
